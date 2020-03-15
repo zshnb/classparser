61ec1e3e0c1bfed9a34a26bd664951e0f98c9289
zshnb <a857681664@gmail.com>
2020-03-15 20:15:19 +0800

    make FileUtil method static

--- src/main/kotlin/com/hdjnb/classparser/FileUtil.kt
+++ src/main/kotlin/com/hdjnb/classparser/FileUtil.kt
@@ -3,8 +3,10 @@ package com.hdjnb.classparser
 import org.apache.commons.io.FileUtils
 
 class FileUtil {
-    fun readBytes(filePath: String): ByteArray {
-        val classFile = FileUtils.getFile(filePath)
-        return FileUtils.readFileToByteArray(classFile)
+    companion object {
+        fun readBytes(filePath: String): ByteArray {
+            val classFile = FileUtils.getFile(filePath)
+            return FileUtils.readFileToByteArray(classFile)
+        }
     }
 }
