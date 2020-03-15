5805c28685590c14447dddcc7eaec8faf556c737
zshnb <a857681664@gmail.com>
2020-03-15 16:28:18 +0800

    init

---
+++ src/main/kotlin/com/hdjnb/Hello.kt
@@ -0,0 +1,6 @@
+package com.hdjnb
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/hdjnb/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.hdjnb
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
