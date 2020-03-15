289a085523baa47dbe0bc898da7b4eed88d94810
zshnb <a857681664@gmail.com>
2020-03-15 20:32:20 +0800

    read utf byte method

--- src/main/kotlin/com/hdjnb/classparser/ByteReader.kt
+++ src/main/kotlin/com/hdjnb/classparser/ByteReader.kt
@@ -1,5 +1,26 @@
 package com.hdjnb.classparser
 
-class ByteReader(private val fileUtil: FileUtil) {
+class ByteReader(private val filePath: String) {
+    private var bytes: MutableList<Byte> = FileUtil.readBytes(filePath).toMutableList()
 
+    @ExperimentalStdlibApi
+    fun readU1(): Byte = bytes.removeFirst()
+
+    fun readU2(): List<Byte> {
+        val u2 = bytes.slice(IntRange(0, 1))
+        bytes.removeAll(u2)
+        return u2
+    }
+
+    fun readU4(): List<Byte> {
+        val u4 = bytes.slice(IntRange(0, 3))
+        bytes.removeAll(u4)
+        return u4
+    }
+
+    fun readU8(): List<Byte> {
+        val u8 = bytes.slice(IntRange(0, 7))
+        bytes.removeAll(u8)
+        return u8
+    }
 }
