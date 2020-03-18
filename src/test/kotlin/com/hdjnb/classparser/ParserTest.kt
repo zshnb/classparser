9d80b4c6b10730e783893cc45e9f6b7c97f5c735
zshnb <a857681664@gmail.com>
2020-03-18 22:31:40 +0800

    test parse major version

--- src/test/kotlin/com/hdjnb/classparser/ParserTest.kt
+++ src/test/kotlin/com/hdjnb/classparser/ParserTest.kt
@@ -1,5 +1,6 @@
 package com.hdjnb.classparser
 
+import org.hamcrest.Matchers
 import org.hamcrest.core.IsEqual
 import org.junit.*
 import org.junit.Assert.assertThat
@@ -14,9 +15,18 @@ class ParserTest {
         byteReader = ByteReader(TestUtil.filePath)
         parser = Parser(byteReader)
     }
+
     @Test
     fun testParseMagicNumber() {
         val magicNumberInfo = parser.parseMagicNumber()
         assertThat(magicNumberInfo.value, IsEqual.equalTo("CAFEBABE"))
+    }
+
+    @Test
+    fun testParseMinorAndMajorVersion() {
+        testParseMagicNumber()
+        val minorVersionInfo = parser.parseMinorVersion()
+        val majorVersionInfo = parser.parseMajorVersion()
+        assertThat(majorVersionInfo.value.toDouble(), Matchers.greaterThan(45.3))
     }
 }
