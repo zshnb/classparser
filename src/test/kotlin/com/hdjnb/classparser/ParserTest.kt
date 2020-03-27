739c7730bdbd93ca1d860a6338c8b1dd30a3d69c
zshnb <a857681664@gmail.com>
2020-03-27 21:33:51 +0800

    complete constant pool test

--- src/test/kotlin/com/hdjnb/classparser/ParserTest.kt
+++ src/test/kotlin/com/hdjnb/classparser/ParserTest.kt
@@ -40,13 +40,76 @@ class ParserTest {
             when {
                 it is ConstantClassInfo -> {
                     assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_CLASS_INFO))
-                    assertThat(it.nameIndex, Matchers.lessThan(65535))
+                    assertThat(it.nameIndex, Matchers.lessThanOrEqualTo(65535))
                 }
                 it is ConstantUtf8Info -> {
                     assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_UTF8_INFO))
                     assertThat(it.length, Matchers.greaterThan(0))
                     assertThat(it.bytes, Matchers.isA(String::class.java))
                 }
+                it is ConstantIntegerInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_INTEGER_INFO))
+                    assertThat(it.bytes, Matchers.isA(Int::class.java))
+                }
+                it is ConstantFloatInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_FLOAT_INFO))
+                    assertThat(it.bytes, Matchers.isA(Float::class.java))
+                }
+                it is ConstantLongInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_LONG_INFO))
+                    assertThat(it.bytes, Matchers.isA(Long::class.java))
+                }
+                it is ConstantDoubleInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_DOUBLE_INFO))
+                    assertThat(it.bytes, Matchers.isA(Double::class.java))
+                }
+                it is ConstantStringInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_STRING_INFO))
+                    assertThat(it.index, Matchers.lessThanOrEqualTo(65535))
+                }
+                it is ConstantFieldRefInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_FIELD_REF_INFO))
+                    assertThat(it.classInfoIndex, Matchers.lessThanOrEqualTo(constants.size))
+                    assertThat(it.nameAndTypeInfoIndex, Matchers.lessThanOrEqualTo(constants.size))
+                }
+                it is ConstantMethodRefInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_METHOD_REF_INFO))
+                    assertThat(it.classInfoIndex, Matchers.lessThanOrEqualTo(constants.size))
+                    assertThat(it.nameAndTypeInfoIndex, Matchers.lessThanOrEqualTo(constants.size))
+                }
+                it is ConstantInterfaceMethodRefInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_INTERFACE_METHOD_REF_INFO))
+                    assertThat(it.classInfoIndex, Matchers.lessThanOrEqualTo(constants.size))
+                    assertThat(it.nameAndTypeInfoIndex, Matchers.lessThanOrEqualTo(constants.size))
+                }
+                it is ConstantNameAndTypeInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_NAME_AND_TYPE_INFO))
+                    assertThat(it.nameIndex, Matchers.lessThanOrEqualTo(constants.size))
+                    assertThat(it.typeIndex, Matchers.lessThanOrEqualTo(constants.size))
+                }
+                it is ConstantMethodHandleInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_METHOD_HANDLE_INFO))
+                    assertThat(it.referenceKind, Matchers.isIn(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)))
+                    assertThat(it.referenceIndex, Matchers.lessThanOrEqualTo(constants.size))
+                }
+                it is ConstantMethodTypeInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_METHOD_TYPE_INFO))
+                    assertThat(it.descriptorIndex, Matchers.lessThanOrEqualTo(constants.size))
+                }
+                it is ConstantDynamicInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_DYNAMIC_INFO))
+//                    assertThat(it.bootstrapMethodAttrIndex)
+                    assertThat(it.nameAndTypeIndex, Matchers.lessThanOrEqualTo(constants.size))
+                }
+                it is ConstantInvokeDynamicInfo -> {
+                    assertThat(it.tag, Matchers.equalTo(Tag.CONSTANT_INVOKE_DYNAMIC_INFO))
+//                    assertThat(it.bootstrapMethodAttrIndex)
+                    assertThat(it.nameAndTypeIndex, Matchers.lessThanOrEqualTo(constants.size))
+                }
+                /*
+                * TODO I just want parse class file which version is below JDK8, so it's unnecessary to test module and
+                *      package info
+                * */
             }
         }
     }
